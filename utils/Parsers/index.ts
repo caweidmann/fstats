7d2dc2904afa35431ca1cde3fa1c21388f259007
Cornelius Weidmann <caweidmann.dev@gmail.com>
2026-02-06 19:10:13 +0100

    fix: Type

--- utils/Parsers/index.ts
+++ utils/Parsers/index.ts
@@ -1,9 +1,10 @@
 import type { Parser } from '@/types'
+import { SupportedParsers } from '@/types-enums'
 
 import { CapitecSavings } from './Capitec'
 import { ComdirectGiro } from './Comdirect'
 
-export const AVAILABLE_PARSERS: Record<'capitec__savings' | 'comdirect__giro', Parser> = {
-  capitec__savings: CapitecSavings,
-  comdirect__giro: ComdirectGiro,
-}
+export const AVAILABLE_PARSERS = {
+  [SupportedParsers.CAPITEC]: CapitecSavings,
+  [SupportedParsers.COMDIRECT_GIRO]: ComdirectGiro,
+} satisfies Record<Exclude<SupportedParsers, 'unknown'>, Parser>
