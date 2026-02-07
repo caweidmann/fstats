6949d8099404fb66d4135d617dfff82e262a8a99
Cornelius Weidmann <caweidmann.dev@gmail.com>
2026-02-07 18:18:53 +0100

    fix: Loading states

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -1,11 +1,18 @@
 'use client'
 
+import { PageWrapper } from '@/components'
+import { useStorage } from '@/context/Storage'
 import { useFileHelper } from '@/hooks'
 
 import { EmptyStatsPage, StatsPage } from './components'
 
 const Page = () => {
+  const { isLoading } = useStorage()
   const { selectedFiles } = useFileHelper()
+
+  if (isLoading) {
+    return <PageWrapper />
+  }
 
   if (!selectedFiles.length) {
     return <EmptyStatsPage />
