a416191cccee467ff416514659a128e53f1d0fc0
Cornelius Weidmann <caweidmann.dev@gmail.com>
2026-02-07 18:55:00 +0100

    fix: Wording

--- app/settings/components/UserPreferences/index.tsx
+++ app/settings/components/UserPreferences/index.tsx
@@ -6,7 +6,7 @@ const Component = () => {
   return (
     <Card sx={{ borderRadius: 2, p: 3 }}>
       <Typography variant="caption" component="p" sx={{ mb: 2 }}>
-        User preferences
+        Preferences
       </Typography>
 
       <Typography component="div" sx={{ fontSize: 14, mb: 1 }}>
