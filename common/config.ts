4efa408990be8746374f8954ab688bdd2f09f6b4
Cornelius Weidmann <caweidmann.dev@gmail.com>
2026-02-09 10:19:56 +0100

    fix: Vars

--- common/config.ts
+++ common/config.ts
@@ -3,5 +3,7 @@ import packageJson from '../package.json'
 export const CONFIG = {
   APP_VERSION: packageJson.version,
   ENABLE_CONSOLE_LOGGING: process.env.NODE_ENV === 'development',
-  TAN_STACK_QUERY_DEVTOOLS: false,
+  ENABLE_TANSTACK_QUERY_DEVTOOLS: false,
+  ENABLE_ANALYTICS: true,
+  ENABLE_SPEED_INSIGHTS: false,
 } as const
