520f5e0a90469dae88c1afc2ceba13620918be64
Cornelius Weidmann <caweidmann.dev@gmail.com>
2026-02-12 14:05:47 +0100

    fix: Add helper to detect duplicate parsers

--- parsers/index.ts
+++ parsers/index.ts
@@ -3,9 +3,12 @@ import { ParserId } from '@/types-enums'
 
 import { CapitecSavings } from './Capitec'
 import { ComdirectGiro, ComdirectVisa } from './Comdirect'
+import { checkUniqueParserIds } from './helper'
 
 export const AVAILABLE_PARSERS = {
   [ParserId.CAPITEC]: CapitecSavings,
   [ParserId.COMDIRECT_GIRO]: ComdirectGiro,
   [ParserId.COMDIRECT_VISA]: ComdirectVisa,
 } satisfies Record<ParserId, Parser>
+
+checkUniqueParserIds(AVAILABLE_PARSERS)
