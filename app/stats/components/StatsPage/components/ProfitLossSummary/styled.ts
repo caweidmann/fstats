efff403e92b903a6a282bf10eae3f2f0023a43cd
Cornelius Weidmann <caweidmann.dev@gmail.com>
2026-02-14 17:58:42 +0100

    chore: Remove dead code

--- app/stats/components/StatsPage/components/ProfitLossSummary/styled.ts
+++ app/stats/components/StatsPage/components/ProfitLossSummary/styled.ts
@@ -14,21 +14,5 @@ export const ui = (theme: Theme) => {
       display: 'flex',
       alignItems: 'center',
     },
-
-    taxCard: {
-      p: 2.5,
-      borderRadius: 2,
-      border: '1px solid',
-      borderColor: 'divider',
-      height: '100%',
-    },
-
-    progressBar: {
-      height: 8,
-      borderRadius: 4,
-      '& .MuiLinearProgress-bar': {
-        borderRadius: 4,
-      },
-    },
   }
 }
