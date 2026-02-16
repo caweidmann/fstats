ae000293bc10653ece8ea56f5464c95c4b127b70
Cornelius Weidmann <caweidmann.dev@gmail.com>
2026-02-16 18:55:22 +0100

    feat: Add logic to get unique timestamps

--- utils/Date/formatters.ts
+++ utils/Date/formatters.ts
@@ -1,60 +1,8 @@
-import { format, isValid, parse } from 'date-fns'
+import { format, isValid, parse, parseISO } from 'date-fns'
 
 import { UserLocale } from '@/types-enums'
-import { MISC } from '@/common'
 
 import { getDateFnsLocale } from '../Date'
-
-/**
- * Converts a date string or date object to "yyyy-MM-dd" format
- *
- * @param date - Date string or date object
- * @param options - Optional options
- * @param options_formatFrom - The format to convert from when supplying a date string - defaults to "yyyyMMdd"
- *
- * @returns Date string in "yyyy-MM-dd" format
- */
-export const toSystemDate = (date: Date | string, options?: { formatFrom?: string }): string => {
-  if (!date) {
-    throw new Error('Must be Date or string')
-  }
-
-  const dateFormat = options?.formatFrom ?? MISC.SYSTEM_DATE_FORMAT
-  const dateObject = date instanceof Date ? date : parse(date, dateFormat, new Date())
-
-  if (isValid(dateObject)) {
-    return format(dateObject, MISC.SYSTEM_DATE_FORMAT)
-  }
-
-  throw new Error('Invalid date')
-}
-
-/**
- * Converts a date string to a JS Date object
- *
- * @param date - Date string or date object to convert
- * @param options - Optional options
- * @param options_formatFrom - The format to convert from when supplying a date string - defaults to "yyyy-MM-dd"
- *
- * @returns Date string in `displayDate` format or `formatTo` format.
- */
-export const toDate = (date: string, options?: { formatFrom?: string }): Date => {
-  let dateObject
-  const dateFormat = options?.formatFrom ?? MISC.SYSTEM_DATE_FORMAT
-
-  try {
-    dateObject = parse(date, dateFormat, new Date())
-  } catch (err) {
-    console.error('Invalid date:', err)
-    throw new Error('Invalid date')
-  }
-
-  if (isValid(dateObject)) {
-    return dateObject
-  }
-
-  throw new Error('Invalid date')
-}
 
 /**
  * Converts a date string or date object to "dd/MM/yyyy" or `options.formatTo` format
@@ -62,7 +10,7 @@ export const toDate = (date: string, options?: { formatFrom?: string }): Date =>
  * @param date - Date string or date object to convert
  * @param options - Optional options
  * @param options_formatFrom - The format to convert from when supplying a date string - defaults to "yyyy-MM-dd"
- * @param options_formatTo - The format to return - defaults to "dd/MM/yyyy"
+ * @param options_formatTo - The format to return - defaults to ISO date format
  *
  * @returns Date string in `displayDate` format or `formatTo` format.
  */
@@ -77,8 +25,8 @@ export const toDisplayDate = (
   if (!date) {
     throw new Error('Must be Date or string')
   }
-  const dateFormatFrom = options?.formatFrom ?? MISC.SYSTEM_DATE_FORMAT
-  const dateObject = date instanceof Date ? date : parse(date, dateFormatFrom, new Date())
+  const dateObject =
+    date instanceof Date ? date : options?.formatFrom ? parse(date, options.formatFrom, new Date()) : parseISO(date)
 
   if (isValid(dateObject)) {
     return format(dateObject, options.formatTo, { locale: getDateFnsLocale(locale) })
