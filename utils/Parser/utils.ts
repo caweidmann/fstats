3a8748562cdb55e7fcd165f122dad7312a6a5c4d
Cornelius Weidmann <caweidmann.dev@gmail.com>
2026-02-20 12:38:39 +0100

    Revert "refactor: Move method"
    This reverts commit 8335f434e5fe46c9061c3b24d2cf754a7b22c0c5.

--- utils/Parser/utils.ts
+++ utils/Parser/utils.ts
@@ -1,10 +1,6 @@
-import { formatISO, isValid, parse } from 'date-fns'
-
-import type { ColDef, CreateParserParams, Parser, RowAccessor, RowData, Transaction } from '@/types'
+import type { Parser } from '@/types'
 import { ParserId } from '@/types-enums'
 import { MISC } from '@/common'
-import { getUniqueTimestamps } from '@/utils/Date'
-import { Big } from '@/lib/w-big'
 
 import {
   capitec__savings,
@@ -15,7 +11,6 @@ import {
   ing__giro__wb,
   lloyds__current,
 } from './banks'
-import { getCsvSortOrder, isArrayEqual, resolveGetter } from './helper'
 
 export const AVAILABLE_PARSERS: Record<ParserId, Parser> = {
   // South African Banks
@@ -61,86 +56,5 @@ export const getParserName = (value: ParserId | null): { short: string; long: st
     short: 'Invalid',
     long: 'Invalid parser',
     alt: 'Invalid parser',
-  }
-}
-
-export const createParser = <T extends ColDef, Id extends ParserId>({
-  id,
-  bankName,
-  accountType,
-  currency,
-  headerRowIndex,
-  columns,
-  dateFormat,
-  getters,
-}: CreateParserParams<T, Id>): Parser<Id> => {
-  const keys = Object.keys(columns) as (keyof T)[]
-  const headers = Object.values(columns)
-
-  const wrapRow = (row: RowData): RowAccessor<T> => ({
-    get: (key) => row[keys.indexOf(key)].trim(),
-  })
-
-  const getDate = resolveGetter(getters.date)
-  const getDescription = resolveGetter(getters.description)
-  const getValue = resolveGetter(getters.value)
-
-  return {
-    id,
-    bankName,
-    accountType,
-    currency,
-    headerRowIndex,
-    columns,
-    dateFormat,
-
-    detect: (input) => {
-      const dataRows = input.data.slice(headerRowIndex + 1)
-
-      if (!dataRows.length) {
-        return false
-      }
-
-      const headersMatch = isArrayEqual(input.data[headerRowIndex], headers)
-      const rowsValid = dataRows.every((row) => row.length === headers.length)
-
-      return headersMatch && rowsValid
-    },
-
-    parse: (input) => {
-      const rowsToParse = input.data.slice(headerRowIndex + 1).filter((row) => row.length === headers.length)
-
-      const parsedRows = rowsToParse.map((rawRow) => {
-        const row = wrapRow(rawRow)
-        const value = getValue(row) || '0'
-        const parsedDate = parse(getDate(row), dateFormat, new Date())
-
-        if (!isValid(parsedDate)) {
-          throw new Error('Invalid date')
-        }
-
-        return {
-          parsedDate,
-          description: getDescription(row),
-          value,
-        }
-      })
-
-      const dates = parsedRows.map((row) => row.parsedDate)
-      const sortOrder = getCsvSortOrder(dates)
-      const uniqueTimestamps = getUniqueTimestamps({ dates, dateFormat, sortOrder })
-
-      return parsedRows.map((row, index) => {
-        const data: Transaction = {
-          date: formatISO(uniqueTimestamps[index]),
-          description: row.description,
-          value: row.value,
-          currency,
-          category: Big(row.value).gte(0) ? 'Income' : 'Expense', // FIXME: Add cats parser
-        }
-
-        return data
-      })
-    },
   }
 }
