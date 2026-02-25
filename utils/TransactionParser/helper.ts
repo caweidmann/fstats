641f8c21b48c3c8dde70b34e7b324462d9ae4b68
Cornelius Weidmann <caweidmann.dev@gmail.com>
2026-02-25 16:34:40 +0100

    fix: Add parser keywords

--- utils/TransactionParser/helper.ts
+++ utils/TransactionParser/helper.ts
@@ -1,32 +1,89 @@
 import type { KeywordRule } from '@/types'
 
 export const RAW_INCOME_KEYWORD_RULES: KeywordRule[] = [
-  // { category: 'INC', keywords: [] },
+  {
+    category: 'PAS_03',
+    keywords: ['Interest Received', 'INTEREST ON CREDIT BALANCE'],
+  },
 
-  { category: 'PAS_03', keywords: ['Interest Received'] },
+  {
+    category: 'OTH_06',
+    keywords: ['PAYMENT-THANK YOU', 'PAYMENT - THANK YOU', 'AMAZON PRIM', 'AMZN Mktp', 'STEAM PURCHASE'],
+  },
 
-  // { category: 'OTH', keywords: [] },
-
-  { category: 'TFI_01', keywords: ['Visa-Kartenabrechnung'] },
+  {
+    category: 'TFI_01',
+    keywords: ['Visa-Kartenabrechnung'],
+  },
 ]
 
 export const RAW_EXPENSE_KEYWORD_RULES: KeywordRule[] = [
-  { category: 'HOU_03', keywords: ['TELEKOM'] },
-  { category: 'HOU_07', keywords: ['IKEA'] },
+  {
+    category: 'HOU_03',
+    keywords: ['TELEKOM'],
+  },
+  {
+    category: 'HOU_07',
+    keywords: ['IKEA'],
+  },
 
-  // { category: 'TRN', keywords: [] },
+  {
+    category: 'TRN_04',
+    keywords: ['Parkgarage'],
+  },
 
-  // { category: 'GRO', keywords: [] },
+  {
+    category: 'FAM_05',
+    keywords: ['GOOGLE*YOUTUBE', 'GOOGLE *YouTube', 'Google YouTube Dublin', 'YouTube'],
+  },
+  {
+    category: 'FAM_06',
+    keywords: ['STEAM PURCHASE', 'STEAMGAMES.COM'],
+  },
 
-  // { category: 'HLT', keywords: [] },
+  {
+    category: 'FAM_09',
+    keywords: ['AMZN Mktp', 'AMAZON PRIM'],
+  },
 
-  // { category: 'FAM', keywords: [] },
+  {
+    category: 'FIN_02',
+    keywords: ['Internet Banking Payment: Fnb Cheque Account'],
+  },
+  {
+    category: 'FIN_04',
+    keywords: ['Internet Banking Payment: Luno (stb)'],
+  },
+  {
+    category: 'FIN_05',
+    keywords: [
+      'Monthly Account Admin Fee',
+      'International Processing Card Purchase Fee',
+      'INTEREST',
+      '#INT PYMT FEE',
+      '#CREDIT CARD ACCOUNT FEE',
+      '#CREDIT FACILITY SERVICE FEE',
+    ],
+  },
 
-  { category: 'FIN_05', keywords: ['Monthly Account Admin Fee', 'International Processing Card Purchase Fee'] },
+  {
+    category: 'BUS_03',
+    keywords: [
+      'CALUDE.AI',
+      'GITHUB',
+      'Google CLOUD',
+      'GOOGLE*CLOUD',
+      'DIGITALOCEAN.COM',
+      'NAME-CHEAP.COM',
+      'DNH*GODADDY',
+      'Microsoft',
+    ],
+  },
 
-  { category: 'BUS_03', keywords: ['CALUDE.AI', 'GITHUB'] },
-
-  { category: 'TFO_01', keywords: ['Summe Monatsabrechnung Visa-Karte', 'Internet Banking Transfer'] },
+  {
+    category: 'TFO_01',
+    keywords: ['Summe Monatsabrechnung Visa-Karte', 'Internet Banking Transfer'],
+  },
 ]
 
 const toLowercaseKeywordRules = (rules: KeywordRule[]): KeywordRule[] => {
